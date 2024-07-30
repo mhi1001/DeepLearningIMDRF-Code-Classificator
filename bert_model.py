5385679f1a2e3b84578353be1e4746c2ea6b380a
mhi1001 <35073349+mhi1001@users.noreply.github.com>
2024-07-30 02:13:59 +0200

    berttt - todo arguments to train/ save and load

--- bert_model.py
+++ bert_model.py
@@ -6,24 +6,24 @@ from transformers import BertTokenizer, BertForSequenceClassification, Trainer, 
 from sklearn.metrics import accuracy_score, precision_recall_fscore_support
 import matplotlib.pyplot as plt
 import json
+import argparse
+import os
 
-# not working
-# TODO: env vars
-# nvidia gpu for faster training
-# WHY ISSSSS IT NOT WRKINGGGG
+######################Dont touch -  use gpu for faster training###################
 print(f"CUDA available: {torch.cuda.is_available()}")
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 if torch.cuda.is_available():
-    device = torch.device("cuda")
     print(f"GPU name: {torch.cuda.get_device_name(0)}")
     print(f"Current device: {torch.cuda.current_device()}")
-else:
-    device = torch.device("cpu")
 print(f"Using device: {device}")
+######################################################################################
 
 with open('message.json', 'r', encoding='utf-8') as file:
     data = json.load(file)
 
-# 1 entry for 1 definition in  the datasett
+# 1 entry for 1 definition in  the dataset
+#{'code': 'A0101', 'term': 'Patient-Device Incompatibility', 'definition': 'The patient experienced severe discomfort due to the device not aligning with their anatomy.'}
+#{'code': 'A0101', 'term': 'Patient-Device Incompatibility', 'definition': "Complications arose because the device did not interact well with the patient's physiological condition."}
 expanded_data = []
 for entry in data:
     code = entry['code']
@@ -91,7 +91,6 @@ def compute_metrics(pred):
 training_args = TrainingArguments(
     output_dir='./results',
     # Number of epochs
-    # TODO: 10 with gpu 
     num_train_epochs=10,  
     per_device_train_batch_size=8,
     per_device_eval_batch_size=8,
@@ -100,7 +99,7 @@ training_args = TrainingArguments(
     weight_decay=0.01,
     logging_dir='./logs',
     report_to="none",
-    # evaluate at each epoch
+    # Evaluate at each epoch
     evaluation_strategy="epoch",  
 )
 
@@ -114,7 +113,7 @@ trainer = Trainer(
     compute_metrics=compute_metrics,
 )
 
-# train and evaluate the model
+# Train and evaluate the model
 trainer.train()
 results = trainer.evaluate()
 print("Evaluation results:", results)
@@ -125,23 +124,21 @@ print("Evaluation results:", results)
 def get_top_5_predictions(text):
     tokens = tokenizer.encode(text, add_special_tokens=True, max_length=512, truncation=True)
 
-    # force model to use GPU...
-    # TODO: still not working 
+    # Force model to use GPU...
     input_ids = torch.tensor([tokens]).to(device)  
     model.to(device)  
-    #####
-
-
     model.eval()
+
     # Output top 5 codes for each query and their respective probability for debugging 
     with torch.no_grad():
         outputs = model(input_ids)
+
     probabilities = torch.nn.functional.softmax(outputs.logits, dim=1)
     top_5_probs, top_5_indices = torch.topk(probabilities, 5)
     top_5_codes = [list(label_to_id.keys())[i] for i in top_5_indices[0]]
     return top_5_codes, top_5_probs[0]
 
-# interaction loop
+# Interaction loop
 while True:
     text_input = input("Enter a description (or 'exit' to quit): ")
     if text_input.lower() == 'exit':
